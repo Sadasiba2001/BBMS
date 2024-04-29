937bc9cb58dab0135b7957624c6283bcc0596ecf
Sadasiba Baliyarsingh <113602822+Sadasiba2001@users.noreply.github.com>
2024-04-29 15:04:07 +0530

    Update settings.py

--- BBMS/settings.py
+++ BBMS/settings.py
@@ -73,14 +73,11 @@ WSGI_APPLICATION = 'BBMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'ORCL',
-        'USER': 'c##practice',
-        'PASSWORD': 'practice',
-        'HOST': 'localhost',
-        'PORT': '1521',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
